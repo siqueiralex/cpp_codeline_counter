d7dc3d5cf08ac2366ea28060c4aeae56c15292f0
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-10-04 19:55:51 -0300

    eliminate spaces tests passed

--- src/codeline_counter.cpp
+++ src/codeline_counter.cpp
@@ -5,6 +5,15 @@ bool only_spaces(std::string s) {
     return s.find_first_not_of(' ') == s.npos;
 }
 
-void elim_spaces(std::string *s){
-    
+void elim_spaces(std::string *s) {
+    using namespace std;
+    int begin = (*s).find_first_not_of(' ');
+    int end = (*s).find_last_not_of(' ');
+    if (begin != -1  && end != -1){
+        int size = end-begin+1;
+        *s = (*s).substr(begin, size);
+    } else {
+        string empty("");
+        (*s) = empty;
+    }
 }
