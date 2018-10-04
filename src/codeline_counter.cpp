1f375dea70b4edffda1c6e41185590cf8bdcd7ab
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-10-04 19:48:23 -0300

    only spaces tests passed

--- src/codeline_counter.cpp
+++ src/codeline_counter.cpp
@@ -1,6 +1,6 @@
 // Copyright Alex Siqueira
 #include "../include/codeline_counter.hpp"
 
-bool only_spaces(std::string s){
-    return true;
+bool only_spaces(std::string s) {
+    return s.find_first_not_of(' ') == s.npos;
 }
