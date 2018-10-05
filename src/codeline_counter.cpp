d0a947e3367d8a1b6f81fc37ef12cab1a5c34922
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-10-04 23:53:32 -0300

    adding comments to cpp file

--- src/codeline_counter.cpp
+++ src/codeline_counter.cpp
@@ -1,10 +1,20 @@
 // Copyright Alex Siqueira
 #include "../include/codeline_counter.hpp"
 
+
+/*
+* Função que verifica se a string contém apenas espaços
+* (descrição mais completa no arquivo de headers 'codeline_counter.hpp')
+*/
 bool only_spaces(std::string s) {
     return s.find_first_not_of(' ') == s.npos;
 }
 
+
+/*
+* Função que elimina os espaços no início e final da string
+* (descrição mais completa no arquivo de headers 'codeline_counter.hpp')
+*/
 void elim_spaces(std::string *s) {
     int begin = (*s).find_first_not_of(' ');
     int end = (*s).find_last_not_of(' ');
@@ -17,6 +27,11 @@ void elim_spaces(std::string *s) {
     }
 }
 
+
+/*
+* Função que verifica se a string não-vazia contém apenas comentários 
+* (descrição mais completa no arquivo de headers 'codeline_counter.hpp')
+*/
 bool only_comments(std::string s) {
     std::string end_comm("*/");
     if (s.find("/") != s.npos &&
@@ -29,16 +44,31 @@ bool only_comments(std::string s) {
     return false;
 }
 
+
+/*
+* Função que verifica se a string contém delimitador de final de comentário
+* (descrição mais completa no arquivo de headers 'codeline_counter.hpp')
+*/
 bool ends_comment(std::string s) {
     return (s.find("/") != s.npos &&
         (s[s.find("/")+1] == '/' || s[s.find_last_of("/")-1] == '*'));
 }
 
+
+/*
+* Função que verifica se a string contém delimitador de início de comentário
+* (descrição mais completa no arquivo de headers 'codeline_counter.hpp')
+*/
 bool initiates_comment(std::string s) {
     return (s.find("/") != s.npos &&
         (s[s.find("/")+1] == '/' || s[s.find("/")+1] == '*'));
 }
 
+
+/*
+* Função que verifica se a string indica que a próxima linha é de comentário
+* (descrição mais completa no arquivo de header 'codeline_counter.hpp')
+*/
 bool next_line_is_comment(std::string s) {
     std::string double_slash("//");
     return (initiates_comment(s) && !ends_comment(s)) ||
@@ -46,6 +76,12 @@ bool next_line_is_comment(std::string s) {
             s.find_last_of('\\') == (s.size()-1));
 }
 
+
+/*
+* Função que verifica conta a quantidade de linhas de código em uma string
+* que representa um código corretamente desenvolvido em C++.
+* (descrição mais completa no arquivo de header 'codeline_counter.hpp')
+*/
 int count_code_lines(std::string s) {
     int count = 0;
     bool inside_comment = false;
